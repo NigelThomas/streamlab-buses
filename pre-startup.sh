a60df0e4f805e07550edd8304dbaa0b2f484a0f4
NigelThomas <nigel.cl.thomas@gmail.com>
2019-12-19 14:51:10 +0000

    add project-specific startup

---
+++ pre-startup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Script executed (if present) before starting the pumps in a project
+
+echo ...  execute project specific startup for buses
+$SQLSTREAM_HOME/demo/data/buses/start.sh
+
